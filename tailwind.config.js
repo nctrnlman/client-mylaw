6298145aee912d6c545af45892558376e9a31349
auliamnaufal <auliamnaufal@gmail.com>
2023-09-02 18:32:44 +0700

    Style: change daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#005289",
+          secondary: "#897500",
+          accent: "#004960",
+          neutral: "#222f35",
+          "base-100": "#f4f4fa",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
