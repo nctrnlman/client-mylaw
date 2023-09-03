3a8c9a5514f66464d1f930a1ae63d9e50ba4776c
auliamnaufal <auliamnaufal@gmail.com>
2023-09-03 15:03:58 +0700

    Feat: create about us section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AboutUs from "./components/AboutUs";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
       <div>
         <Navbar />
         <Hero />
+        <AboutUs />
       </div>
     </>
   );
