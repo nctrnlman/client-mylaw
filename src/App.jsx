c8c4bff2c81a5d828519133161cf81446f510842
auliamnaufal <auliamnaufal@gmail.com>
2023-09-03 15:12:36 +0700

    Feat: create company achievement summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import AboutUs from "./components/AboutUs";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Summary from "./components/Summary";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Navbar />
         <Hero />
+        <Summary />
         <AboutUs />
       </div>
     </>
---
+++ src/components/Summary.jsx
@@ -0,0 +1,21 @@
+const Summary = () => {
+  return (
+    <div className="w-screen bg-primary">
+      <div className="p-5 max-w-7xl flex justify-around items-center mx-auto text-white">
+        <p>
+          <span className="text-xl font-bold">120+</span> Clients
+        </p>
+        <div className="badge badge-white badge-xs"></div>
+        <p>
+          <span className="text-xl font-bold">50+</span> Services
+        </p>
+        <div className="badge badge-white badge-xs"></div>
+        <p>
+          <span className="text-xl font-bold">99.8%</span> Cases Settled
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
