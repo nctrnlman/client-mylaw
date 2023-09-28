1b9c70e0179180365b757cf3f768f85945e05b6f
auliamnaufal <auliamnaufal@gmail.com>
2023-09-28 18:09:40 +0700

    add vision and mission

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -44,14 +44,26 @@ const Services = () => {
         </div>
         <img src={aboutUsImg} alt="" className="w-1/2 justify-self-end" />
       </div>
-      <div className="max-w-7xl grid grid-cols-2 grid-rows-1 justify-items-center mt-20">
-        <div>
+      <div className="max-w-7xl mx-auto grid grid-cols-2 grid-rows-1 justify-items-center mt-20 mb-32">
+        <div className="text-center">
           <h2 className="text-4xl font-bold mb-5">Our Vision</h2>
-          <p></p>
+          <p>
+            With our extensive expertise in corporate, commercial, civil law,
+            and debt recovery, Mylaw is the preferred choice for landlords,
+            tenants, businesses of all sizes, private foundations, individuals,
+            and families seeking to safeguard their rights and interests. We're
+            here to stand by your side and provide the legal support you need.
+          </p>
         </div>
-        <div>
-          <h2 className="text-4xl font-bold mb-5">Our Vision</h2>
-          <p></p>
+        <div className="text-center">
+          <h2 className="text-4xl font-bold mb-5">Our Mission</h2>
+          <p>
+            With our extensive expertise in corporate, commercial, civil law,
+            and debt recovery, Mylaw is the preferred choice for landlords,
+            tenants, businesses of all sizes, private foundations, individuals,
+            and families seeking to safeguard their rights and interests. We're
+            here to stand by your side and provide the legal support you need.
+          </p>
         </div>
       </div>
       <div className="w-screen bg-base-100 text-black">
