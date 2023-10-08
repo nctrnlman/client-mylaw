039b6d44c0a2a444ffe80bd04d0c9cf65bc81941
auliamnaufal <auliamnaufal@gmail.com>
2023-10-09 00:48:20 +0700

    Ref: change services and navbar bg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,34 +3,46 @@ import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="navbar max-w-7xl mx-auto">
-      <div className="flex-1">
-        <Link to="/" className="normal-case cursor-pointer">
-          <img
-            src={logo}
-            alt="MyLaw Logo"
-            className="w-24 h24 object-contain object-center"
-          />
-        </Link>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <Link to="/about-us">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/client">Our Clients</Link>
-          </li>
-          <li>
-            <Link to="/article">News & Insight</Link>
-          </li>
-          <li>
-            <Link to="/contact-us">Contact Us</Link>
-          </li>
-        </ul>
+    <div className="bg-base-100">
+      <div className="navbar max-w-7xl mx-auto">
+        <div className="flex-1">
+          <Link to="/" className="normal-case cursor-pointer">
+            <img
+              src={logo}
+              alt="MyLaw Logo"
+              className="w-24 h24 object-contain object-center"
+            />
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link className="" to="/about-us">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="" to="/services">
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link className="" to="/client">
+                Our Clients
+              </Link>
+            </li>
+            <li>
+              <Link className="" to="/article">
+                News & Insight
+              </Link>
+            </li>
+            <li>
+              <Link className="" to="/contact-us">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -38,7 +38,8 @@ const Services = () => {
     <div
       className="hero min-h-screen"
       style={{
-        backgroundImage: "url(./src/assets/img-service.jpg)",
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1505664063603-28e48ca204eb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80)",
       }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
