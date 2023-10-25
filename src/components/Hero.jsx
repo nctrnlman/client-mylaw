b0b1404454328ecd02890d7a9a2f5c930f3fe6c8
auliamnaufal <auliamnaufal@gmail.com>
2023-10-25 23:47:51 +0700

    Feat: install countup animation & implement it in summary component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <div
-      className="hero min-h-screen"
+      className="hero min-h-[calc(91vh-140px)]"
       style={{
         backgroundImage:
           "url(https://images.unsplash.com/photo-1505488387362-48bc38155987?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2942&q=80",
