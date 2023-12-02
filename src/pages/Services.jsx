e4c8d06b98ec6f7979ad9a664132dece2a75a0c3
Rhazes Devino <rhazesd@gmail.com>
2023-12-02 13:20:55 +0700

    revisi

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -220,7 +220,53 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="py-10 md:py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+      <div className="py-5 md:py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          <div className="mb-5 lg:w-3/4 flex flex-col">
+            <h1 className="text-4xl lg:text-5xl font-bold pb-5 py-2 text-primary after:content-[''] after:block after:py-1 after:border-b after:border-primary after:scale-x-0 hover:after:scale-x-100 after:transition">
+              Restructuring, Turnaround, and Insolvency
+            </h1>
+            <p className="md:text-lg text-justify">
+              We are a leading and strategic business partner to collect and
+              recover companies commercial debts effectively without
+              jeopardizing their relationships with customers. Additionally, we
+              assure our clients with no money upfront. They pay only when they
+              are paid.
+            </p>
+          </div>
+          <div className="flex justify-end">
+            {/* SVG will be hidden on mobile screens */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-[80%] h-auto text-secondary hidden lg:block"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+              />
+            </svg>
+          </div>
+        </div>
+        <div className="container mx-auto my-8 text-sm md:text:lg">
+          <ul className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {restructurinTurnaroundInsolvency.map((service) => (
+              <li
+                key={service}
+                className="border md:text-lg border-secondary p-4 items-center text-black text-center  rounded transition-transform hover:scale-105"
+              >
+                {service}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div className="py-5 md:py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
         <div className="flex flex-col lg:flex-row">
           <div className="mb-5 lg:w-3/4 flex flex-col">
             <h1 className="text-4xl lg:text-5xl font-bold pb-5 py-2 text-primary after:content-[''] after:block after:py-1 after:border-b after:border-primary after:scale-x-0 hover:after:scale-x-100 after:transition">
@@ -265,7 +311,7 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+      <div className="py-5 md:py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
         <div className="flex flex-col lg:flex-row">
           <div className="mb-5 lg:w-3/4 flex flex-col">
             <h1 className="text-4xl lg:text-5xl font-bold pb-5 py-2 text-primary after:content-[''] after:block after:py-1 after:border-b after:border-primary after:scale-x-0 hover:after:scale-x-100 after:transition">
@@ -314,7 +360,7 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+      <div className="py-5 md:py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
         <div className="flex flex-col lg:flex-row">
           <div className="mb-5 lg:w-3/4 flex flex-col">
             <h1 className="text-4xl lg:text-5xl font-bold pb-5 py-2 text-primary after:content-[''] after:block after:py-1 after:border-b after:border-primary after:scale-x-0 hover:after:scale-x-100 after:transition">
@@ -363,52 +409,7 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
-        <div className="flex flex-col lg:flex-row">
-          <div className="mb-5 lg:w-3/4 flex flex-col">
-            <h1 className="text-4xl lg:text-5xl font-bold pb-5 py-2 text-primary after:content-[''] after:block after:py-1 after:border-b after:border-primary after:scale-x-0 hover:after:scale-x-100 after:transition">
-              Restructuring, Turnaround, and Insolvency
-            </h1>
-            <p className="md:text-lg text-justify">
-              We are a leading and strategic business partner to collect and
-              recover companies commercial debts effectively without
-              jeopardizing their relationships with customers. Additionally, we
-              assure our clients with no money upfront. They pay only when they
-              are paid.
-            </p>
-          </div>
-          <div className="flex justify-end">
-            {/* SVG will be hidden on mobile screens */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-[80%] h-auto text-secondary hidden lg:block"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
-              />
-            </svg>
-          </div>
-        </div>
-        <div className="container mx-auto my-8 text-sm md:text:lg">
-          <ul className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
-            {restructurinTurnaroundInsolvency.map((service) => (
-              <li
-                key={service}
-                className="border md:text-lg border-secondary p-4 items-center text-black text-center  rounded transition-transform hover:scale-105"
-              >
-                {service}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-      <div className="py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+      <div className="py-5 md:py-20 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
         <div className="flex flex-col lg:flex-row">
           <div className="mb-5 lg:w-3/4 flex flex-col">
             <h1 className="text-4xl lg:text-5xl font-bold pb-5 py-2 text-primary after:content-[''] after:block after:py-1 after:border-b after:border-primary after:scale-x-0 hover:after:scale-x-100 after:transition">
